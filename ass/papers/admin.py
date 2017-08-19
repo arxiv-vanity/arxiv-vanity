b1d9a6f2822b24eb6cab1d32097d1dfd45f8623b
Ben Firshman <ben@firshman.co.uk>
2017-08-19 15:36:05 +0300

    Better paper and render admin

--- ass/papers/admin.py
+++ ass/papers/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+import json
 from .models import Paper, Render
 
 
 class PaperAdmin(admin.ModelAdmin):
     actions = ['download', 'render']
-    list_display = ['title', 'is_downloaded', 'is_rendered']
+    list_display = ['title', 'is_downloaded', 'render_state']
 
     def is_downloaded(self, obj):
         return bool(obj.source_file.name)
     is_downloaded.boolean = True
 
-    def is_rendered(self, obj):
-        # TODO(bfirsh): take into account render state
-        return obj.renders.count() > 0
-    is_rendered.boolean = True
+    def render_state(self, obj):
+        try:
+            render = obj.renders.latest()
+        except Render.DoesNotExist:
+            return ""
+        return '<a href="../render/{}/">{}</a>'.format(render.id, render.state)
+    render_state.allow_tags = True
 
     def download(self, request, queryset):
         for paper in queryset:
@@ -30,6 +35,22 @@ admin.site.register(Paper, PaperAdmin)
 
 class RenderAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'paper', 'state', 'container_id')
+    # The fields except the ones we're formatting
+    RENDER_FIELDS = [
+        f.name for f in Render._meta.get_fields()
+        if f.name not in ['container_logs', 'container_inspect']
+    ] + ['formatted_container_logs', 'formatted_container_inspect']
+    fields = RENDER_FIELDS
+    readonly_fields = RENDER_FIELDS
+
+    def formatted_container_logs(self, obj):
+        return mark_safe("<pre>{}</pre>".format(obj.container_logs))
+    formatted_container_logs.short_description = 'Container logs'
+
+    def formatted_container_inspect(self, obj):
+        formatted = json.dumps(obj.container_inspect, indent=2)
+        return mark_safe("<pre>{}</pre>".format(formatted))
+    formatted_container_inspect.short_description = 'Container inspect'
 
 
 admin.site.register(Render, RenderAdmin)
