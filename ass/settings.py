d238ea3f5108e5bae722a8c64fb2cdf43cc05084
Ben Firshman <ben@firshman.co.uk>
2017-08-28 15:15:58 +0300

    Prevent widows in headings

--- ass/settings.py
+++ ass/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'typogrify',
     'ass.feedback',
     'ass.papers',
     'ass.scraper',
