0167a6b597fc61b83b2cd872949ea2eecea1e3a8
Ben Firshman <ben@firshman.co.uk>
2017-08-28 13:38:26 +0300

    Add simple pagination
    Fixes #12

--- ass/papers/views.py
+++ ass/papers/views.py
@@ -6,7 +6,7 @@ from .models import Paper, Render
 
 class PaperListView(ListView):
     model = Paper
-    paginate_by = 25
+    paginate_by = 50
 
     def get_queryset(self):
         qs = super(PaperListView, self).get_queryset()
