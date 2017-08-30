db200b6449f0261f582dbfc7b75e77f7ec2ad8ae
Ben Firshman <ben@firshman.co.uk>
2017-08-30 16:04:37 +0300

    Add dates to paper list

--- ass/settings.py
+++ ass/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'typogrify',
     'ass.feedback',
     'ass.papers',
