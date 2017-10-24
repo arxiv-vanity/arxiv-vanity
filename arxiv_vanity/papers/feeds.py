230c4ce6861655bfb36f4b721e48bd41c5324754
Ben Firshman <ben@firshman.co.uk>
2017-10-24 12:30:45 +0300

    Add RSS feed

---
+++ arxiv_vanity/papers/feeds.py
@@ -0,0 +1,27 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+from .models import Paper
+
+class LatestPapersFeed(Feed):
+    title = "Arxiv Vanity – Latest machine learning papers"
+    link = "/papers/"
+    description = "Latest machine learning papers from Arxiv rendered as web pages"
+
+    def items(self):
+        qs = Paper.objects.machine_learning().has_successful_render()
+        return qs.order_by('-updated')[:25]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.summary
+
+    def item_author_name(self, item):
+        try:
+            return ', '.join(author['name'] for author in item.authors)
+        except KeyError:
+            return ''
+
+    def item_pubdate(self, item):
+        return item.updated
--- arxiv_vanity/urls.py
+++ arxiv_vanity/urls.py
@@ -18,11 +18,13 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from .feedback.views import submit_feedback
+from .papers.feeds import LatestPapersFeed
 from .papers.views import HomeView, PaperListView, paper_detail, paper_convert, paper_render_state, render_update_state
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^papers/$', PaperListView.as_view(), name='paper_list'),
+    url(r'^papers/feed/$', LatestPapersFeed(), name='paper_feed'),
     url(r'^papers/(?P<arxiv_id>[^/]+)/$', paper_detail, name='paper_detail'),
     url(r'^papers/(?P<arxiv_id>[^/]+)/render-state/$', paper_render_state, name='paper_render_state'),
     url(r'^renders/(?P<pk>\d+)/update-state/$', render_update_state, name='render_update_state'),
