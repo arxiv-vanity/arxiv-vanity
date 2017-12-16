6ecf8776d2924c713c2456129113e145de68e9f1
Ben Firshman <ben@firshman.co.uk>
2017-12-16 18:08:42 +0530

    Revert "Upgrade to Django 2.0"
    This reverts commit 6c2171f2db8dd2683e41081f8156c805c2251622.
    
    There is a bug displaying literal new lines.

--- arxiv_vanity/urls.py
+++ arxiv_vanity/urls.py
@@ -1,26 +1,36 @@
 """URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
 from .feedback.views import submit_feedback
 from .papers.feeds import LatestPapersFeed
 from .papers.views import HomeView, PaperListView, paper_detail, paper_convert, paper_render_state, render_update_state
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('papers/', PaperListView.as_view(), name='paper_list'),
-    path('papers/feed/', LatestPapersFeed(), name='paper_feed'),
-    path('papers/<arxiv_id>/', paper_detail, name='paper_detail'),
-    path('papers/<arxiv_id>/render-state/', paper_render_state, name='paper_render_state'),
-    path('renders/<int:pk>/update-state/', render_update_state, name='render_update_state'),
-    path('convert/', paper_convert, name='paper_convert'),
-    path('submit-feedback/', submit_feedback),
-    path('admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^papers/$', PaperListView.as_view(), name='paper_list'),
+    url(r'^papers/feed/$', LatestPapersFeed(), name='paper_feed'),
+    url(r'^papers/(?P<arxiv_id>[^/]+)/$', paper_detail, name='paper_detail'),
+    url(r'^papers/(?P<arxiv_id>[^/]+)/render-state/$', paper_render_state, name='paper_render_state'),
+    url(r'^renders/(?P<pk>\d+)/update-state/$', render_update_state, name='render_update_state'),
+    url(r'^convert/$', paper_convert, name='paper_convert'),
+    url(r'^submit-feedback/$', submit_feedback),
+    url(r'^admin/', admin.site.urls),
 ]
 
 # Serve uploaded files in development
