ce014ad579c7da30fab3b652ab3291bd6270d5da
Ben Firshman <ben@firshman.co.uk>
2018-01-12 18:53:56 +0200

    Remove version from Arxiv ID for new papers
    Store version separately in database.
    
    Migration to add version to Paper.

--- arxiv_vanity/papers/tests/test_models.py
+++ arxiv_vanity/papers/tests/test_models.py
@@ -7,16 +7,16 @@ from .utils import create_paper, create_render, create_source_file_bulk_tarball
 
 class PaperTest(TestCase):
     def test_get_source_url(self):
-        paper = create_paper(arxiv_id='1708.03312v1')
-        self.assertEqual(paper.get_source_url(), 'https://arxiv.org/e-print/1708.03312v1')
+        paper = create_paper(arxiv_id='1708.03313')
+        self.assertEqual(paper.get_source_url(), 'https://arxiv.org/e-print/1708.03313')
 
     def test_get_https_arxiv_url(self):
-        paper = create_paper(arxiv_id='http://arxiv.org/abs/1708.03312v1')
-        self.assertEqual(paper.get_https_arxiv_url(), 'https://arxiv.org/abs/1708.03312v1')
+        paper = create_paper(arxiv_url='http://arxiv.org/abs/1708.03313')
+        self.assertEqual(paper.get_https_arxiv_url(), 'https://arxiv.org/abs/1708.03313')
 
     def test_get_https_pdf_url(self):
-        paper = create_paper(pdf_url='http://arxiv.org/pdf/1708.03312v1')
-        self.assertEqual(paper.get_https_pdf_url(), 'https://arxiv.org/pdf/1708.03312v1')
+        paper = create_paper(pdf_url='http://arxiv.org/pdf/1708.03313')
+        self.assertEqual(paper.get_https_pdf_url(), 'https://arxiv.org/pdf/1708.03313')
 
     def test_is_renderable(self):
         paper = create_paper(source_file=None)
--- arxiv_vanity/scraper/arxiv_ids.py
+++ arxiv_vanity/scraper/arxiv_ids.py
@@ -7,4 +7,11 @@ def remove_version_from_arxiv_id(arxiv_id):
     match = ARXIV_ID_RE.match(arxiv_id)
     if not match:
         return arxiv_id, None
-    return match.group(1), match.group(2)
+    return match.group(1), int(match.group(2))
+
+
+ARXIV_URL_RE = re.compile(r'v(\d+)$')
+
+
+def remove_version_from_arxiv_url(url):
+    return ARXIV_URL_RE.sub('', url)
