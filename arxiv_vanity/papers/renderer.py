997654e2186c9f5b2606d0622ca42396b54d0155
Ben Firshman <ben@firshman.co.uk>
2018-01-16 14:48:27 +0200

    Only make output directory when rendering locally

--- arxiv_vanity/papers/renderer.py
+++ arxiv_vanity/papers/renderer.py
@@ -43,10 +43,6 @@ def render_paper(source, output_path, webhook_url=None):
     """
     Render a source directory using Engrafo.
     """
-    try:
-        os.makedirs(output_path)
-    except FileExistsError:
-        pass
     client = create_client()
 
     labels = {}
@@ -63,6 +59,11 @@ def render_paper(source, output_path, webhook_url=None):
         environment['AWS_S3_REGION_NAME'] = settings.AWS_S3_REGION_NAME
     # Development
     else:
+        try:
+            os.makedirs(output_path)
+        except FileExistsError:
+            pass
+
         # HACK(bfirsh): MEDIA_ROOT is an absolute path to something on
         # the host machine. We need to make this relative to a mount inside the
         # Docker container.
