8ef9de9501cc1f5289d08bccc4bb25182ef80407
Ben Firshman <ben@firshman.co.uk>
2018-06-21 19:00:15 +0300

    Make source_file a raw_id_field
    1.4 million source files ain't gonna work

--- arxiv_vanity/papers/admin.py
+++ arxiv_vanity/papers/admin.py
@@ -29,6 +29,7 @@ class PaperAdmin(admin.ModelAdmin):
     list_filter = [HasSuccessfulRenderListFilter]
     list_per_page = 250
     search_fields = ['arxiv_id', 'title']
+    raw_id_fields = ("source_file",)
 
     def has_source_file(self, obj):
         return bool(obj.source_file)
