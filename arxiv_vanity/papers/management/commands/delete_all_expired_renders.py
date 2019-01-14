858404821d46c38d207a066258a63fa345f9512a
Ben Firshman <ben@firshman.co.uk>
2019-01-14 14:36:08 -0600

    Batch expired render deletion

--- arxiv_vanity/papers/management/commands/delete_all_expired_renders.py
+++ arxiv_vanity/papers/management/commands/delete_all_expired_renders.py
@@ -6,11 +6,21 @@ class Command(BaseCommand):
     help = 'Deletes output of all expired renders'
 
     def handle(self, *args, **options):
-        for render in Render.objects.expired().iterator():
-            try:
-                render.delete_output()
-            except FileNotFoundError:
-                print(f"❌  Render {render.id} already deleted", flush=True)
-            else:
-                print(f"✅  Render {render.id} deleted", flush=True)
+        pointer = 0
+        batch_size = 1000
+        qs = Render.objects.expired()
+
+        while True:
+            # Batch because Django just seems to consume loads of memory and lock up
+            batch = qs.filter(id__gt=pointer).order_by('id')[:batch_size]
+            if not batch:
+                break
+            for render in batch:
+                pointer = render.id
+                try:
+                    render.delete_output()
+                except FileNotFoundError:
+                    print(f"❌  Render {render.id} already deleted", flush=True)
+                else:
+                    print(f"✅  Render {render.id} deleted", flush=True)
 
