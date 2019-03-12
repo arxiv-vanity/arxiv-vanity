7961ceb12e3e3ef83537b3a15081131a1a9816a4
Ben Firshman <ben@firshman.co.uk>
2019-03-12 14:57:02 -0400

    Move sponsors to footer

--- arxiv_vanity/urls.py
+++ arxiv_vanity/urls.py
@@ -19,7 +19,6 @@ urlpatterns = [
     path('convert/', paper_convert, name='paper_convert'),
     path('submit-feedback/', submit_feedback),
     path('stats/', stats),
-    path('sponsors/', TemplateView.as_view(template_name='sponsors.html'), name='sponsors'),
     path('admin/', admin.site.urls),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
