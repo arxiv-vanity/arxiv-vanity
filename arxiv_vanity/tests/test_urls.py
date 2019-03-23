ccc7270da5ed991e3352cb5e95e9aedf06ff4032
Jakub Kukul <jakub.kukul@gmail.com>
2019-03-23 22:35:13 +0800

    Add basic test for url resolver.

---
+++ arxiv_vanity/tests/test_urls.py
@@ -0,0 +1,15 @@
+from django.urls import resolve
+from django.test import TestCase
+
+
+class UrlsTest(TestCase):
+
+    def test_urls(self):
+        resolver = resolve('/papers/1703.07815/')
+        self.assertEquals(resolver.view_name, 'paper_detail')
+
+        resolver = resolve('/pdf/1703.07815/')
+        self.assertEquals(resolver.view_name, 'django.views.generic.base.RedirectView')
+
+        resolver = resolve('/papers/1703.07815/render-state/')
+        self.assertEquals(resolver.view_name, 'paper_render_state')
