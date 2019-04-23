ef0f4c3234da215e3eb1b5b9baf6851f603bdc88
Ben Firshman <ben@firshman.co.uk>
2019-04-23 12:55:08 -0700

    Remove deprecated assertEquals

--- arxiv_vanity/tests/test_urls.py
+++ arxiv_vanity/tests/test_urls.py
@@ -6,16 +6,16 @@ class UrlsTest(TestCase):
 
     def test_urls(self):
         resolver = resolve('/papers/1703.07815/')
-        self.assertEquals(resolver.view_name, 'paper_detail')
+        self.assertEqual(resolver.view_name, 'paper_detail')
 
         resolver = resolve('/pdf/1703.07815/')
-        self.assertEquals(resolver.view_name, 'django.views.generic.base.RedirectView')
+        self.assertEqual(resolver.view_name, 'django.views.generic.base.RedirectView')
 
         resolver = resolve('/papers/1703.07815/render-state/')
-        self.assertEquals(resolver.view_name, 'paper_render_state')
+        self.assertEqual(resolver.view_name, 'paper_render_state')
 
         resolver = resolve('/papers/astro-ph/0601001/')
-        self.assertEquals(resolver.view_name, 'paper_detail')
+        self.assertEqual(resolver.view_name, 'paper_detail')
 
         resolver = resolve('/papers/astro-ph/0601001/render-state/')
-        self.assertEquals(resolver.view_name, 'paper_render_state')
+        self.assertEqual(resolver.view_name, 'paper_render_state')
