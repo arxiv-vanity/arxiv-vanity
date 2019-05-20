68dfe9c86ec1d2042b3f1eef21738300a3c6caf2
Ben Firshman <ben@firshman.co.uk>
2019-05-20 13:17:31 +0100

    Add command to mark failed renders as expired

---
+++ arxiv_vanity/papers/management/commands/mark_failed_renders_as_expired.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from ...models import Render
+
+
+class Command(BaseCommand):
+    help = 'Marks all renders as expired so they will be rerendered'
+
+    def handle(self, *args, **options):
+        qs = Render.objects.defer("container_inspect", "container_logs").failed().not_expired()
+        qs.force_expire()
+        print(f"Done")
