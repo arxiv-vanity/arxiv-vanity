11ee3968ddf124606454677bcd29c560345ae77b
Ben Firshman <ben@firshman.co.uk>
2019-06-27 12:55:20 -0700

    Add debugging to update_render_state

--- arxiv_vanity/papers/management/commands/update_render_state.py
+++ arxiv_vanity/papers/management/commands/update_render_state.py
@@ -7,6 +7,9 @@ class Command(BaseCommand):
     help = 'Sync the state of renders in the database with what is on Docker'
 
     def handle(self, *args, **options):
+        print("Updating state...")
         Render.objects.update_state()
+        print("Updating expired status...")
         Render.objects.update_is_expired()
+        print("Removing long running containers...")
         remove_long_running_containers()
