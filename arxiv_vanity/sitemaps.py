30a9a13b8e369f7911fc2088155a69f8606335c8
Ben Firshman <ben@firshman.co.uk>
2020-01-23 12:17:04 -0800

    Limit number of sitemap entries per page
    heroku timeout

--- arxiv_vanity/sitemaps.py
+++ arxiv_vanity/sitemaps.py
@@ -5,6 +5,7 @@ from .papers.models import Paper
 class PaperSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
+    limit = 5000
 
     def items(self):
         return Paper.objects.all()
