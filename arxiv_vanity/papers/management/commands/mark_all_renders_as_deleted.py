f2245e941f39f1634e5cb1451c5e838fba92600d
Ben Firshman <ben@firshman.co.uk>
2020-01-27 11:58:26 -0800

    Remove dupe management command

--- arxiv_vanity/papers/management/commands/mark_all_renders_as_deleted.py
+++ arxiv_vanity/papers/management/commands/mark_all_renders_as_deleted.py
@@ -3,7 +3,7 @@ from ...models import Render
 
 
 class Command(BaseCommand):
-    help = "Marks all renders as deleted so they will be rerendered"
+    help = "Marks all renders as deleted so they will be rerendered. WARNING! This is old and will likely not work any longer. Also very slow..."
 
     def handle(self, *args, **options):
         qs = Render.objects.defer("container_inspect", "container_logs").not_deleted()
